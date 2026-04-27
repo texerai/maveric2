b967f094017c8a7a6210a1201366aa50122dda6a
Yesmurat <yesmurat2006@gmail.com>
2026-04-27 16:23:42 -0400

    untracked .vscode, __pycache__, and stuff folders

--- stuff/mdu/obj_stall/tb_mdu_stall.d
+++
@@ -1,6 +0,0 @@
-tb_mdu_stall.o: ../tb_mdu_stall.cpp Vmdu.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
