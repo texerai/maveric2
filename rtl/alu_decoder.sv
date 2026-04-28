a644a64425eb773c094baab6d9cabdcbd72fad4e
Yesmurat <yesmurat2006@gmail.com>
2026-04-28 10:10:15 -0400

    removed mul instruction decoding; restored the original CSR decoding code on lines 69-76.

--- rtl/alu_decoder.sv
+++ rtl/alu_decoder.sv
@@ -64,33 +64,16 @@ module alu_decoder
                     default: alu_control_o = 5'b00000;                               // Default to ADD.
                 endcase
 
-            // R Type M extension.
-            // 3'b100:
-                // case (func3_i)
-                //     3'b000: alu_control_o = 5'b01111;  // MUL.
-                //     3'b001: alu_control_o = 5'b10000;  // MULH.
-                //     3'b010: alu_control_o = 5'b10001;  // MULSHU.
-                //     3'b011: alu_control_o = 5'b10010;  // MULHU.
-                //     default: alu_control_o = 5'b00000; // Default to ADD.
-                // endcase
-
-            // R Type W M extansion.
-            // 3'b101:
-                // case (func3_i)
-                //     3'b000: alu_control_o = 5'b10111;  // MULW.
-                //     default: alu_control_o = 5'b00000; // Default to ADD.
-                // endcase
-
             // CSR.
             // 3'b100:
-                // case (func3_i[1:0])
-                //     2'b01: alu_control_o = 5'b10000;
-                //     2'b10: alu_control_o = 5'b10001;
-                //     2'b11: alu_control_o = 5'b10010;
-                //     default: begin
-                //         alu_control_o   = '0;
-                //     end
-                // endcase
+            //     case (func3_i[1:0])
+            //         2'b01: alu_control_o = 5'b10000;
+            //         2'b10: alu_control_o = 5'b10001;
+            //         2'b11: alu_control_o = 5'b10010;
+            //         default: begin
+            //             alu_control_o   = '0;
+            //         end
+            //     endcase
 
             default: begin
                 alu_control_o = '0;
